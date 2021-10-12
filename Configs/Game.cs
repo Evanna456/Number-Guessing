acef367493584f05ed629acb4170e3b8be5a76a7
Jojohn Tabal <steampunk852@yahoo.com>
2021-10-12 21:00:53 +0800

    save options

---
+++ Configs/Game.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+
+namespace WPF_Number_Guessing_Game.Configs
+{
+    public class GameConfigJSON
+    {
+        public int min_range { get; set; }
+        public int max_range { get; set; }
+        public int no_chances { get; set; }
+    }
+    class Game
+    {
+        Configs.Directories myDirectories = new Configs.Directories();
+        public void gameConfigCreate()
+        {
+            string base_directory = myDirectories.base_directory;
+            myDirectories.configDirectory();
+
+            string file_path = Path.Combine(base_directory + "/Configs/", "game.json");
+
+            if (File.Exists(file_path) == false)
+            {
+
+                GameConfigJSON config = new GameConfigJSON();
+                config = new GameConfigJSON
+                {
+                    min_range = Int32.Parse("1"),
+                    max_range = Int32.Parse("100"),
+                    no_chances = Int32.Parse("7"),
+                };
+
+                string json_string = JsonSerializer.Serialize(config);
+                File.WriteAllText(base_directory + "/Configs/game.json", json_string);
+
+            }
+
+        }
+
+        string string_range;
+        int int_string_range_length;
+        string no_chances;
+        string first_digit;
+        int int_first_digit;
+        int range;
+
+        public void gameConfigUpdate(string min_range, string max_range)
+        {
+
+            gameConfigCreate();
+            string base_directory = myDirectories.base_directory;
+
+            int int_min_range = Int32.Parse(min_range);
+            int int_max_range = Int32.Parse(max_range);
+
+            range = int_max_range - int_min_range;
+            string_range = range.ToString();
+            int_string_range_length = string_range.Length;
+
+            first_digit = string_range.Substring(0, 1);
+            int_first_digit = Int32.Parse(first_digit);
+
+
+            GameConfigJSON config = new GameConfigJSON();
+            config = new GameConfigJSON
+            {
+                min_range = int_min_range,
+                max_range = int_max_range,
+                no_chances = Int32.Parse(no_chances),
+            };
+
+            string json_string = JsonSerializer.Serialize(config);
+            File.WriteAllText(base_directory + "/Configs/game.json", json_string);
+
+        }
+     
+
+        }
+}
--- Views/Options.xaml.cs
+++ Views/Options.xaml.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WPF_Number_Guessing_Game.Configs;
 
 namespace WPF_Number_Guessing_Game.Views
 {
@@ -18,22 +13,104 @@ namespace WPF_Number_Guessing_Game.Views
     /// </summary>
     public partial class Options : Page
     {
+        private Configs.Directories myDirectories = new Configs.Directories();
+        private Configs.Game myGameConfigs = new Configs.Game();
+
         public Options()
         {
             InitializeComponent();
         }
 
+        public void load(object sender, RoutedEventArgs e)
+        {
+            string base_directory = myDirectories.base_directory;
+            myDirectories.configDirectory();
+            myGameConfigs.gameConfigCreate();
+
+            string data = File.ReadAllText(base_directory + "/Configs/game.json");
+            GameConfigJSON json = JsonSerializer.Deserialize<GameConfigJSON>(data);
+            int min_range = json.min_range;
+            int max_range = json.max_range;
+
+            MinRangeTextbox.Text = min_range.ToString();
+            MaxRangeTextbox.Text = max_range.ToString();
+        }
+
         public void navigateToHome(object sender, RoutedEventArgs e)
         {
-
             NavigationService.Navigate(new Uri("Views/Index.xaml", UriKind.Relative));
-
         }
-        public void navigateToOptions(object sender, RoutedEventArgs e)
+
+        public void showAboutWindow(object sender, RoutedEventArgs e)
         {
+            AboutWindow myAboutWindow = new AboutWindow();
+            myAboutWindow.Show();
+        }
 
-            NavigationService.Navigate(new Uri("Views/Options.xaml", UriKind.Relative));
+        public void saveBtnClick(object sender, RoutedEventArgs e)
+        {
+            string base_directory = myDirectories.base_directory;
+            SaveOptionsBtn.IsEnabled = false;
+            myGameConfigs.gameConfigCreate();
 
+            string string_min_range = MinRangeTextbox.Text;
+            string string_max_range = MaxRangeTextbox.Text;
+
+            int int_min_range = Int32.Parse(string_min_range);
+            int int_max_range = Int32.Parse(string_max_range);
+
+            int range = int_max_range - int_min_range;
+            string string_range = range.ToString();
+
+            int string_range_length = string_range.Length;
+
+            string first_digit = string_range.Substring(0, 1);
+            int int_first_digit = Int32.Parse(first_digit);
+            string no_chances = "0";
+            Boolean saveOption = false;
+
+            if (range <= 10)
+            {
+                no_chances = "3";
+                saveOption = true;
+            }
+            else if(range <= 50)
+            {
+                no_chances = "5";
+                saveOption = true;
+            }else if(range > 51 && range <= 100)
+            {
+                no_chances = "7";
+                saveOption = true;
+            }else if(range > 100)
+            {
+                int total_chances = 3 + int_first_digit ;
+                for (int it = 0;it < string_range_length;it++)
+                {
+                    total_chances = total_chances + Int32.Parse(string_range[it].ToString());
+                }
+
+                no_chances = total_chances.ToString();
+                saveOption = true;
+            }
+
+            if (saveOption == true) {
+
+                GameConfigJSON config = new GameConfigJSON();
+                config = new GameConfigJSON
+                {
+                    min_range = int_min_range,
+                    max_range = int_max_range,
+                    no_chances = Int32.Parse(no_chances),
+                };
+
+                string json_string = JsonSerializer.Serialize(config);
+                File.WriteAllText(base_directory + "/Configs/game.json", json_string);
+
+                SaveOptionsBtn.IsEnabled = true;
+                MessageBox.Show("Option Saved Successfully", "Success");
+
+            }
         }
     }
 }
